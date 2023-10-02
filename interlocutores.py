49b0ef566cb009f6b93bb97a253c2a00e13247c2
Pablo Tinajas <ptinajasr01@gmail.com>
2023-10-02 18:00:40 +0200

    Estructurada INTERLOC

--- interlocutores.py
+++ interlocutores.py
@@ -1,2 +1,56 @@
-interlocutores
+# aplicación de cartas de interlocutores 
+# necesitamos una API potente entre Excel, Word, python y una interfaz visual facilona
 
+import win32com.client
+import datetime
+import tkinter as tk
+from mailmerge import MailMerge
+import locale
+import re
+import os
+from tkinter import *
+from tkinter import messagebox, ttk
+from ttkthemes import ThemedStyle
+from docx import Document
+from docx.shared import Inches
+from docx2pdf import convert
+from tkinter import filedialog
+import PyPDF2
+
+locale.setlocale(locale.LC_ALL, '')
+
+class DocumentEditor:
+    def __init__(self, document_path):
+        self.document_path = document_path
+        self.document = Document(self.document_path)
+
+# en esta clase se escribirá todo lo necesario para poder generar y editar el documento word
+
+class Excel:
+    
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.master.title("Nota de Calculo de ACODALAMIENTO")
+        self.master.geometry("880x930")
+        self.master.configure(background="#F5F5F5")
+        self.pack(fill=tk.BOTH, expand=True)
+        self.create_widgets()
+    
+
+
+class Checkbar(Frame):
+    def __init__(self, parent=None, picks=[], side=LEFT, anchor=W, checkbox_font=None):
+        Frame.__init__(self, parent)
+        self.vars = []
+        self.checkbox_font = checkbox_font if checkbox_font else ("Helvetica", 12)
+
+        for pick in picks:
+            var = IntVar()
+            chk = Checkbutton(self, text=pick, variable=var, font=self.checkbox_font)
+            chk.pack(side=side, anchor=anchor, expand=YES)
+            self.vars.append(var)
+
+    def state(self):
+        return map((lambda var: var.get()), self.vars)
