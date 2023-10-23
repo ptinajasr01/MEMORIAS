b38e21719f842528a7e38042d6c1376675470239
Pablo Tinajas <ptinajasr01@gmail.com>
2023-10-23 09:50:53 +0200

    REF Interlocutores FR
    el código está completo y ahora hace falta modificar las plantillas al francés

--- modificarBeam.py
+++ modificarBeam.py
@@ -1,57 +1,30 @@
-from PyPDF2 import PdfReader, PdfWriter
-from reportlab.pdfgen import canvas
-from reportlab.lib.pagesizes import letter
-from reportlab.lib.units import inch
-from reportlab.lib.utils import ImageReader
-from tkinter import Tk, filedialog
-import io
-import getpass
-username = getpass.getuser()
+from pdf2docx import Converter
+import fitz
+from docx import Document
+from PIL import Image
+from io import BytesIO
 
+# Provide the paths for your PDF and Word files
+pdf_file_path = f"C:\\Users\\PabloTinajas\\Downloads\\N1 INCYE300 L=18.21m.pdf"
+docx_file_path = f"C:\\Users\\PabloTinajas\\Downloads\\N1 INCYE300 L=18.21m.docx"
 
-import PyPDF2
+# Open the PDF file
+pdf_document = fitz.open(pdf_file_path)
 
-def insert_image(pdf_in, img, pdf_out):
+# Create a new Word document
+doc = Document()
 
-  pdf_reader = PdfReader(pdf_in)
-  pdf_writer = PdfWriter()
+# Iterate through each page of the PDF
+for page_number in range(pdf_document.page_count):
+    page = pdf_document.load_page(page_number)
+    image_list = page.get_images(full=True)
+    for img in image_list:
+        xref = img[0]
+        base_image = pdf_document.extract_image(xref)
+        image_data = base_image["image"]
+        image = Image.open(BytesIO(image_data))
+        image.save(f"C:\\Users\\PabloTinajas\\Downloadsimage{page_number}_{img[1]}.png")  # save images as PNG
+        doc.add_picture(f"C:\\Users\\PabloTinajas\\Downloadsimage{page_number}_{img[1]}.png")
 
-  for page_num in range(len(pdf_reader.pages)):
-
-    pdf_page = pdf_reader.pages[page_num]
-    packet = io.BytesIO()
-    can = canvas.Canvas(packet, pagesize=letter)
-
-    # Draw the existing page content first 
-    can.saveState() 
-    can.translate(0,0)
-    can.drawImage(pdf_page, 0, 300)
-    can.restoreState()
-
-    # Draw the new image on top
-    can.drawImage(img, 200, 200)  
-
-    can.save()
-
-    packet.seek(0)  
-    new_pdf = PdfReader(packet)  
-    pdf_writer.add_page(new_pdf.pages[0])
-
-  with open(pdf_out, "wb") as f:
-    pdf_writer.write(f)
-
-def main():
-    root = Tk()
-    root.withdraw()
-    pdf_path = filedialog.askopenfilename()
-    image1 = f"C:\\Users\\{username}\\Incye\\Ingenieria - Documentos\\12_Aplicaciones\\BeamMod\\incyelogo.jpg"
-    image2 = f"C:\\Users\\{username}\\Incye\\Ingenieria - Documentos\\12_Aplicaciones\\BeamMod\\imagen (2).png"
-      # Absolute paths of the images
-
-    output_path = pdf_path.split(".pdf")[0] + "_edited.pdf"
-
-    insert_image(pdf_path, image1, output_path)
-    print(f"New PDF generated at {output_path}")
-    
-if __name__ == "__main__":
-    main()
+# Save the Word document
+doc.save(docx_file_path)
